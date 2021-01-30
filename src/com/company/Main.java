29ce0c74568b7ab2f7f6597171b40577ddfb3d28
Ivan_Dombrovan <Ivan-Dombrovan2016@gmail.com>
2021-01-30 17:56:35 +0200

    Delete element

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,19 @@
 package com.company;
 import java.util.Arrays;
-
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[100];
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        System.out.println("Enter the number you want to delete ");
+        int sc = scanner.nextInt();
         int value = 1;
         for (int i = 0; i < array.length; i++) {
             array[i] = value;
             value++;
-       }
-        System.out.println("Array: " + Arrays.toString(array));
-        for(int i = 2; i < 100; ++i){
-            int count = 0;
-            for(int j = 2; j <=i && count < 2;++j){
-                if(i%j==0){
-                    ++count;
-                }
-            }
-            if(count<2)
-                System.out.println(i);
         }
-
+        System.out.println("Array: " + Arrays.toString(array));
         }
     }
 
